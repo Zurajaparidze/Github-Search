14fcbb0245d22d569621e4552f67da83a17814e6
Zura Japaridze <114393054+Zurajaparidze@users.noreply.github.com>
2023-04-21 00:07:36 +0400

    First Committ

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import WhiteCard from './components/WhiteCard'
+import LightMode from './components/LightMode'
+
+
+function App() {
+  return (
+    <div className="App">
+        <LightMode />
+    </div>
+  );
+}
+
+export default App;
